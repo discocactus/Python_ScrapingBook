a04d3bafc2efddc4eac9d53d7e406362d7b51c82
discocactus <hideshi.honma@gmail.com>
2017-09-06 00:35:48 +0900

    new

---
+++ rest_api_withtweepy.py
@@ -0,0 +1,22 @@
+
+# Tweepyを使ってタイムラインを取得する
+
+import os
+import tweepy
+
+# 環境変数から認証情報を取得する
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+# 認証情報を設定する
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth) # APIクライアントを取得する
+public_tweets = api.home_timeline() # ユーザーのタイムラインを取得する
+
+# 得られるツイートのオブジェクトはTweepyのStatusオブジェクト
+for status in public_tweets:
+    print('@' + status.user.screen_name, status.text) # ユーザー名とツイートを取得する
