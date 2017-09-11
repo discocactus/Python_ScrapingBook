c89a45f5107a88685c18f46a2925cdfb9ce059a8
discocactus <hideshi.honma@gmail.com>
2017-09-12 00:52:29 +0900

    new

---
+++ get_museum.py
@@ -0,0 +1,22 @@
+
+# SPARQLを使って日本の美術館を取得するスクリプト
+
+from SPARQLWrapper import SPARQLWrapper
+
+# SPARQLエンドポイントのURLを指定してインスタンスを作成する
+sparql = SPARQLWrapper('http://ja.dbpedia.org/sparql')
+# 日本の美術館を取得するクエリを設定する。
+# バックスラッシュを含むので、rで始まるraw文字列を使用している
+sparql.setQuery(r'''
+SELECT * WHERE {
+    ?s rdf:type dbpedia-owl:Museum ;
+        prop-ja:所在地 ?address .
+    FILTER REGEX(?address, "^\\p{Han}{2,3}[都道府県]")
+} ORDER BY ?s
+''')
+sparql.setReturnFormat('json') # 取得するフォーマットとしてJSONを指定する
+# query()でクエリを実行し、convert()でレスポンスをパースしてdictを得る
+response = sparql.query().convert()
+
+for result in response['results']['bindings']:
+    print(result['s']['value'], result['address']['value']) # 抽出した変数の値を表示する
---
+++ get_note_contents.py
@@ -0,0 +1,56 @@
+
+# noteのコンテンツを取得する (Ajax通信完了待機対応版)
+
+import sys
+import time # ← 追加
+from selenium import webdriver
+
+
+def main():
+    """
+    メインの処理
+    """
+    
+    driver = webdriver.PhantomJS() # PhantomJSのWebDriverオブジェクトを作成する
+    driver.set_window_size(800, 600) # ウインドウサイズを設定する
+    
+    navigate(driver) # noteのトップページに遷移する
+    posts = scrape_posts(driver) # 文章コンテンツのリストを取得する
+    
+    # コンテンツの情報を表示する
+    for post in posts:
+        print(post)
+        
+        
+def navigate(driver):
+    """
+    目的のページに遷移する
+    """
+    
+    print('Navigating...', file=sys.stderr)
+    driver.get('https://note.mu/') # noteのトップページを開く
+    assert 'note' in driver.title # タイトルに'note'が含まれていることを確認する
+    time.sleep(2)  # 2秒間待つ。 ← この行を追加
+    
+    
+def scrape_posts(driver):
+    """
+    文章コンテンツのURL、タイトル、概要を含むdictのリストを取得する
+    """
+    
+    posts = []
+    
+    # すべての文章コンテンツを表すa要素について反復する
+    for a in driver.find_elements_by_css_selector('a.p-post--basic'):
+        # URL、タイトル、概要を取得して、dictとしてリストに追加する
+        posts.append({
+            'url': a.get_attribute('href'),
+            'title': a.find_element_by_css_selector('h4').text,
+            'description': a.find_element_by_css_selector('.c-post__description').text,
+        })
+        
+    return posts
+
+
+if __name__ == '__main__':
+    main()    
